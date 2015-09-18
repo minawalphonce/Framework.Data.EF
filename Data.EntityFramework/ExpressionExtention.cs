fc3f730a5ff150845d61e992936f738e36913889
mina.alphonce <mina@malphonce.com>
2015-09-18 09:57:21 +0200

    adding And / Or Extentions

---
+++ Data.EntityFramework/ExpressionExtention.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class ExpressionExtention
+{
+    private class ParameterRebinder : ExpressionVisitor
+    {
+        private readonly Dictionary<ParameterExpression, ParameterExpression> map;
+
+        public ParameterRebinder(Dictionary<ParameterExpression, ParameterExpression> map)
+        {
+            this.map = map ?? new Dictionary<ParameterExpression, ParameterExpression>();
+        }
+
+        public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map, Expression exp)
+        {
+            return new ParameterRebinder(map).Visit(exp);
+        }
+
+        protected override Expression VisitParameter(ParameterExpression p)
+        {
+            ParameterExpression replacement;
+            if (map.TryGetValue(p, out replacement))
+            {
+                p = replacement;
+            }
+            return base.VisitParameter(p);
+        }
+    }
+
+    /// <summary>
+    /// Composes new expression bt anding or oring 2 expressions
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="first">The first.</param>
+    /// <param name="second">The second.</param>
+    /// <param name="merge">The merge.</param>
+    /// <returns></returns>
+    private static Expression<T> Compose<T>(Expression<T> first, Expression<T> second, Func<Expression, Expression, Expression> merge)
+    {
+        // build parameter map (from parameters of second to parameters of first)
+        var map = first.Parameters.Select((f, i) => new { f, s = second.Parameters[i] }).ToDictionary(p => p.s, p => p.f);
+
+        // replace parameters in the second lambda expression with parameters from the first
+        var secondBody = ParameterRebinder.ReplaceParameters(map, second.Body);
+
+        // apply composition of lambda expression bodies to parameters from the first expression 
+        return Expression.Lambda<T>(merge(first.Body, secondBody), first.Parameters);
+    }
+
+    /// <summary>
+    /// Ands the first expression with the second expression in a new one.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="first">The first.</param>
+    /// <param name="second">The second.</param>
+    /// <returns></returns>
+    public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
+    {
+        return Compose(first, second, Expression.And);
+    }
+
+    /// <summary>
+    /// Ors the first expression with the second expression in a new one.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="first">The first.</param>
+    /// <param name="second">The second.</param>
+    /// <returns></returns>
+    public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
+    {
+        return Compose(first, second, Expression.Or);
+    }
+}
---
+++ Data.EntityFramework/SpecificationsExtentions.cs
@@ -0,0 +1,17 @@
+﻿namespace Framework.Data.EF
+{
+    public static class SpecificationExtention
+    {
+        public static ISpecification<TEntity> Or<TEntity>(this ISpecification<TEntity> spec1, ISpecification<TEntity> spec2)
+            where TEntity: class
+        {
+            return new Specification<TEntity>(spec1.Expression.Or(spec2.Expression));
+        }
+
+        public static ISpecification<TEntity> And<TEntity>(this ISpecification<TEntity> spec1, ISpecification<TEntity> spec2)
+            where TEntity : class
+        {
+            return new Specification<TEntity>(spec1.Expression.And(spec2.Expression));
+        }
+    }
+}
